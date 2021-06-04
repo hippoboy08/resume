8d9321c0a826c2bf4712e8efabd688731e6bcda8
Hippoboy <hippoboy08@gmail.com>
2021-06-04 19:43:57 +0700

    Added the resume main page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,42 @@
+import { Data } from '../common'
+import {
+  AboutSection,
+  Banner,
+  ContactSection,
+  GlobalStyles,
+  NavBar,
+  ProjectsSection,
+  Section,
+  SkillsSection,
+} from '../components'
+import { NavItemProps } from '../components/NavBar'
+
+const Home = () => {
+  GlobalStyles()
+  const navItems: NavItemProps[] = [
+    { title: 'About', icon: '' },
+    { title: 'Skills', icon: '' },
+    { title: 'Projects', icon: '' },
+    { title: 'Contact', icon: '' },
+  ]
+  return (
+    <>
+      <NavBar navItems={navItems} />
+      <Banner />
+      <Section title='About'>
+        <AboutSection {...Data.about} />
+      </Section>
+      <Section title='Skills'>
+        <SkillsSection {...Data} />
+      </Section>
+      <Section title='Projects'>
+        <ProjectsSection projects={Data.projects} />
+      </Section>
+      {/* <Section title='Contact'> */}
+        <ContactSection contacts={Data.contacts} />
+      {/* </Section> */}
+    </>
+  )
+}
+
+export default Home
